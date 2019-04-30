b61bdbfeea739ee0d55a13fe6ee4632c6d4dda9a
Flynn Yoder <50116778+flynnyoder@users.noreply.github.com>
2019-04-30 11:36:29 -0600

    Percentage Calculator

---
+++ Percent Calculator/random.m
@@ -0,0 +1,12 @@
+%Percentage calculator
+%Flynn Yoder
+%Just for practice
+%12/24/18
+A=input('dividend: ');
+B=input('divisor: ');
+if B==0
+    disp('ERROR: divisor cannot be zero')
+else
+    div=(A)/(B);
+    percent=div*100
+end
